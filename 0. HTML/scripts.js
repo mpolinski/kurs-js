c10e1e922aa2fcc442296daf4f895695e84c3895
mpolinski <mariusz.polinski@gmail.com>
2025-04-24 15:34:23 +0200

    Dodano formularz z nowymn artykułem

--- 0. HTML/scripts.js
+++ 0. HTML/scripts.js
@@ -1,1 +1,38 @@
 console.log("Plik scripts.js załadowany!")
+// let kolor = "zielony";
+
+// switch (kolor) {
+//   case "czerwony":
+//     console.log("Stop!");
+//   case "zielony":
+//     console.log("Idź!");
+//   case "żółty":
+//     console.log("Uwaga!");
+//   default:
+//     console.log("Nieznany kolor");
+// }
+
+
+// let day = prompt("1-7");
+//  switch(day) {
+//     case "1":
+//         alert("poniedzialek!")
+//         break;
+//     default:
+//         alert("nie poniedzialek")
+//  }
+
+//  for (let num = 2; num <= 50; num++) {
+//     let isPrime = true;
+  
+//     for (let i = 2; i < num; i++) {
+//       if (num % i === 0) {
+//         isPrime = false;
+//         break;  // Jeśli znajdziesz dzielnik, przerwij sprawdzanie tej liczby
+//       }
+//     }
+  
+//     if (isPrime) {
+//       console.log(num);  // Wypisz liczbę, jeśli jest pierwsza
+//     }
+//   }
